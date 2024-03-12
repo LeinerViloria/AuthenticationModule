43f47520fb1cb4b1939f48d98112bd24befe20f6
LeinerViloria <lvilora@siesa.com>
2024-03-12 13:35:59 -0500

    Ajustes

--- AuthenticationModule/Access/AuthenticationContext.cs
+++ AuthenticationModule/Access/AuthenticationContext.cs
@@ -7,45 +7,5 @@ namespace AuthenticationModule.Access
     public class AuthenticationContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<User> User {get; set;}
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.1")
-                .HasAnnotation("Relational:MaxIdentifierLength", 63);
-
-            NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
-
-            modelBuilder.Entity("AuthenticationModule.Entities.User", b =>
-                {
-                    b.Property<int>("Rowid")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("integer");
-
-                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Rowid"));
-
-                    b.Property<DateTime>("CreationDate")
-                        .HasColumnType("timestamp with time zone");
-
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<DateTime?>("LastUpdateDate")
-                        .HasColumnType("timestamp with time zone");
-
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasMaxLength(128)
-                        .HasColumnType("character varying(128)");
-
-                    b.HasKey("Rowid");
-
-                    b.HasIndex(new[] { "Email" }, "Ix_User__Email")
-                        .IsUnique();
-
-                    b.ToTable("User");
-                });
-        }
     }
 }
